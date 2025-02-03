f188999c3c323b306393db18f3d56e501aba198f
Michał Ziernik <ziernikmichal@gmail.com>
2025-02-03 03:22:46 +0100

    movie fix

--- app/(main)/app/movies/[movieID]/page.tsx
+++ app/(main)/app/movies/[movieID]/page.tsx
@@ -3,34 +3,42 @@ import Text from "@/lib/components/Text";
 import { logoImage, SlideBackgroundFiller, SlideDescription, SlideElementsContainer, SlideImage } from "@/lib/home/Carousel/Slide/styles";
 import useApi from "@/lib/hooks/useApi";
 import { TMovie } from "@/lib/types/TMovie";
-import { useParams } from "next/navigation"
-import { useEffect, useState } from "react";
+import { useParams, useRouter } from "next/navigation"
+import { useCallback, useEffect, useState } from "react";
 import { episodesContainer, MovieInfoContainer, MovieInfoSlideButtons, MovieInfoSlideContainer, seasonPickerContainer, seasonPickerItem } from "./styles";
 import Button from "@/lib/components/Button";
 import { IconPlayerPlayFilled, IconPlus } from "@tabler/icons-react";
 import Popover from "@/lib/components/Popover";
 import Input from "@/lib/components/Input";
 import Episode from "@/lib/components/Episode";
+import SourceComponent from "@/lib/components/SourceComponent";
+import { linksContainer } from "./episodes/[episodeID]/styles";
 
 export default function MovieInfoPage() {
     const { movieID } = useParams();
     const [movie, setMovie] = useState<TMovie | null>(null);
     const api = useApi();
     const [isTV, setIsTV] = useState(false);
+    const router = useRouter();
     const [season, setSeason] = useState<TMovie['seasons'][0] | undefined>(undefined);
+
+    const refresh = useCallback(async () => {
+        const tv = movieID?.[0] == 't'
+        setIsTV(tv);
+        if (!movieID || !api) return;
+
+        if (!movie || movie.tmdb_id != movieID) {
+            const fetchMovie = await api.get(`/movies/${movieID}`, { params: { sources: !isTV } });
+            console.log(fetchMovie?.data);
+
+            setMovie(fetchMovie?.data);
+            setSeason(fetchMovie?.data?.seasons.find((s: TMovie['seasons'][0]) => s.season_number == 1));
+        }
+    }, [movieID, api]);
 
     useEffect(() => {
         (async () => {
-            setIsTV(movieID?.[0] == 't');
-            if (!movieID || !api) return;
-
-            if (!movie || movie.tmdb_id != movieID) {
-                const fetchMovie = await api.get(`/movies/${movieID}`);
-                console.log(fetchMovie?.data);
-
-                setMovie(fetchMovie?.data);
-                setSeason(fetchMovie?.data?.seasons.find((s: TMovie['seasons'][0]) => s.season_number == 1));
-            }
+            await refresh();
         })();
     }, [movieID, api]);
 
@@ -55,7 +63,7 @@ export default function MovieInfoPage() {
                 </div>
             </div>
             <div className={MovieInfoContainer}>
-                {isTV && <><Text size="xl" weight={600}>Episodes</Text>
+                {isTV ? <><Text size="xl" weight={600}>Episodes</Text>
                     <Popover opened={opened} setOpened={setOpened}>
                         <Popover.Target>
                             <Input width={'300px'} style={{ caretColor: 'transparent' }} readOnly value={`${season?.season_number}: ${season?.name}`} />
@@ -72,6 +80,11 @@ export default function MovieInfoPage() {
                         {
                             movie?.episodes.filter(m => m?.season == season?.season_number).map((episode, index) => <Episode key={index} {...episode} />)
                         }
+                    </div></> : <><Text size="xl" weight={600}>Sources: </Text><div className={linksContainer}>
+                        {movie?.episodes[0]?.sources.map((source, index) => (
+                            <SourceComponent onClick={() => router.push(`/watch/${movieID}/episodes/0,0?source=${source._id}`)} refresh={refresh} key={index} {...source} />
+
+                        ))}
                     </div></>}
             </div>
         </div>
